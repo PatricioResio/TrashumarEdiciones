4d5ebede25bc23a6dadd0635281b540c29cd37f3
Patricio Resio <patricio.resio@gmail.com>
2026-09-08 17:08:27 -0300

    fixing images url in contants and arrays

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,6 +3,6 @@ export const IMG_BRAND =
 export const LOGO_URL =
   "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/logoSinFondo.png?alt=media&token=415a831a-24d9-4507-ab76-8838e7228304";
 export const DEFAULT_PERFIL_FOTO ="https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/DefaultPerfil.png?alt=media&token=0977a220-09e0-4f8d-a668-16dc72fc757e";
-export const TAPA_HIPPIES = "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/Novedad%2FTAPA_HIPPIES.webp?alt=media&token=a597610e-8332-4b40-b492-8e2ca38b1a32";
-export const CONTRATAPA_HIPPIES = "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/Novedad%2FCONTRATAPA_HIPPIES.webp?alt=media&token=2dd6abde-bcfc-4f37-82e2-d1cebcfb4281";
+export const TAPA_HIPPIES = "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/Novedad%2FTAPA_HIPPIES.jpg?alt=media&token=3afceef6-624e-445d-8875-90cb91958fe6";
+export const CONTRATAPA_HIPPIES = "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/Novedad%2FCONTRATAPA_HIPPIES.jpg?alt=media&token=5ada1117-795f-4086-9112-a663cba62649";
 export const ID_PROYECTO_DESTACADO = "hPPVp4z2tMZF4cNA1HOb";
--- src/constants/heroPreload.js
+++ src/constants/heroPreload.js
@@ -3,4 +3,4 @@
  * Mantener alineado con homeArrays[0]; se usa para preload y SEO crítico.
  */
 export const HOME_HERO_LCP_URL =
-  "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/Home1.webp?alt=media&token=26b557aa-9ce3-4a7c-a8b3-f690bccd8a2c";
+  "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/Home3%20(1).webp?alt=media&token=8829c4f6-f6c0-424d-9041-bbfa2d721062";
