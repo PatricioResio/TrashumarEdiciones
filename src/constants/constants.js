d7fd9d3f556fc996a5ba59265008eeee37af135d
Patricio Resio <patricio.resio@gmail.com>
2026-09-16 12:50:25 -0300

    change logo and default perfil foto urls

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -7,8 +7,8 @@ export const IMG_BRAND_RESPONSIVE = {
   
 };
 export const LOGO_URL =
-  "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/logoSinFondo.png?alt=media&token=415a831a-24d9-4507-ab76-8838e7228304";
-export const DEFAULT_PERFIL_FOTO ="https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/DefaultPerfil.png?alt=media&token=0977a220-09e0-4f8d-a668-16dc72fc757e";
+  "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/logoSinFondo.png?alt=media&token=faaf4d9a-b478-40b4-9790-5bd999540944";
+export const DEFAULT_PERFIL_FOTO ="https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/DefaultPerfil.webp?alt=media&token=5134d3ed-d677-47cd-afbc-264b81b09c8d";
 export const TAPA_HIPPIES = "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/Novedad%2FTAPA_HIPPIES.jpg?alt=media&token=3afceef6-624e-445d-8875-90cb91958fe6";
 export const CONTRATAPA_HIPPIES = "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/Novedad%2FCONTRATAPA_HIPPIES.jpg?alt=media&token=5ada1117-795f-4086-9112-a663cba62649";
 export const ID_PROYECTO_DESTACADO = "hPPVp4z2tMZF4cNA1HOb";
