9a7da234b76e88f2179a00af0364c5bf5d601bf8
Patricio Resio <patricio.resio@gmail.com>
2026-09-17 00:45:41 -0300

    fx add 320 px sixe responsiveImages

--- src/constants/Arrays.js
+++ src/constants/Arrays.js
@@ -23,6 +23,7 @@ export const homeArrays = [
   {
     id: 1,
     responsiveSrcs: {
+      320:"https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/HomeCarrousel%2FHome_1_320.webp?alt=media&token=69d66063-7dc8-4821-a6db-f114588b8fff",
      640: "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/HomeCarrousel%2FHome_1_960.webp?alt=media&token=818c425a-8894-41a7-9745-480ba7a2d2db",
      960: "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/HomeCarrousel%2FHome_1_640.webp?alt=media&token=8e4d26db-6629-4fc0-b367-963815e3ec00",
      1280: "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/HomeCarrousel%2FHome_1_1280.webp?alt=media&token=ce0248a3-aee5-4241-a67b-3a5929b4f600",
@@ -37,6 +38,7 @@ export const homeArrays = [
     id: 2,
     h2: "Contacto",
     responsiveSrcs: {
+     320:"https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/HomeCarrousel%2FHome_2_320.webp?alt=media&token=dfd74a03-0490-45f8-90a8-7a193d0da24d",
      640: "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/HomeCarrousel%2FHome_2_640.webp?alt=media&token=a853fc8e-adb8-4bca-b296-b22e572c2f0a",
      960: "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/HomeCarrousel%2FHome_2_960.webp?alt=media&token=8aafb19d-1c8b-415a-849b-b0a6425c2381",
      1280: "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/HomeCarrousel%2FHome_2_1280.webp?alt=media&token=fd240c74-d7d1-4bb2-b521-ebe84ab1be2d",
@@ -50,6 +52,7 @@ export const homeArrays = [
   {
     id: 3,
     responsiveSrcs: {
+      320:"https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/HomeCarrousel%2FHome_3_320.webp?alt=media&token=31d71a77-1229-4907-9ea6-67aeff0e03b7",
      640: "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/HomeCarrousel%2FHome_3_640.webp?alt=media&token=35d294c6-71ab-48a2-9c4c-da22239782db",
      960: "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/HomeCarrousel%2FHome_3_960.webp?alt=media&token=92821b17-7bf0-4ea0-9c2f-e9ca0427db56",
      1280: "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/HomeCarrousel%2FHome_3_1280.webp?alt=media&token=0183def8-a8a8-4b15-a38f-e1e386dd5870",
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,6 @@
 export const IMG_BRAND = "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/BannerCuerpo%2FBannerCuerpo_640.webp?alt=media&token=f2e153d8-f38e-423c-9c1a-1ccfec37f824";
 export const IMG_BRAND_RESPONSIVE = {
+  320:"".
     640: "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/BannerCuerpo%2FBannerCuerpo_640.webp?alt=media&token=f2e153d8-f38e-423c-9c1a-1ccfec37f824",
     960: "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/BannerCuerpo%2FBannerCuerpo_960.webp?alt=media&token=6ee6d72e-ee42-401f-a9ab-e8e77fe61c15",
     1280: "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/BannerCuerpo%2FBannerCuerpo_1280.webp?alt=media&token=f5119545-153d-4215-94cb-af3de18223ca",
@@ -7,7 +8,7 @@ export const IMG_BRAND_RESPONSIVE = {
   
 };
 export const LOGO_URL =
-  "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/logoSinFondo.png?alt=media&token=faaf4d9a-b478-40b4-9790-5bd999540944";
+  "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/logoSinFondo.webp?alt=media&token=de7292fb-fdad-4090-aa59-4a9f7ba63a00";
 export const DEFAULT_PERFIL_FOTO ="https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/DefaultPerfil.webp?alt=media&token=5134d3ed-d677-47cd-afbc-264b81b09c8d";
 export const TAPA_HIPPIES = "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/Novedad%2FTAPA_HIPPIES.jpg?alt=media&token=3afceef6-624e-445d-8875-90cb91958fe6";
 export const CONTRATAPA_HIPPIES = "https://firebasestorage.googleapis.com/v0/b/trashumar-c8bd9.appspot.com/o/Novedad%2FCONTRATAPA_HIPPIES.jpg?alt=media&token=5ada1117-795f-4086-9112-a663cba62649";
